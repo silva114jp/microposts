d7a870b59948c4fa8f36bb42441e6469eda8ffdd
silva114jp <silva114jp@gmail.com>
2016-01-17 12:16:26 +0000

    add user login and logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    # ユーザをメールアドレスから検索
+    @user = User.find_by(email: params[:session][:email].downcase)
+    
+    # ユーザが存在した場合、パスワードが正しいかチェック
+    if @user && @user.authenticate(params[:session][:password])
+      # ユーザーIDをセッション保持し、ユーザの詳細ページへリダイレクトする
+      session[:user_id] = @user.id
+      flash[:info] = "logged in as #{@user.name}"
+      redirect_to @user
+    else
+      # パスワードが誤っている場合、newテンプレートを表示
+      flash[:danger] = 'invalid email/password combination'
+      render 'new'
+    end
+  end
+  
+  def destroy
+    # セッションのユーザIDを破棄し、ルートへリダイレクト
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+end
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,22 @@
+module SessionsHelper
+  
+  #ログイン中のユーザを取得
+  def current_user
+    #本メソッドは、@current_userを返す
+    
+    # ||= とは左の値がfalseまたはnilの場合に、右の値を代入する
+    @current_user ||= User.find_by(id:  session[:user_id])
+  end
+  
+  def logged_in?
+    # current_userが存在するか確認
+    
+    # !!は右の値が存在する場合はtrue、nilの場合はfalse
+    !!current_user
+  end
+  
+  def store_location
+    # リクエストがGETの場合、リクエストのURLを代入
+    session[:forwarding_url] = request.url if request.get?
+  end
+end
